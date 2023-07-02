8df991340d0821d4167c59c680f65ab4f537b516
Martin Paule <67288701+TheBigPaws@users.noreply.github.com>
2023-07-02 21:20:16 +0100

    finalised logic for individual projects, can start copying

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const projectDivs = document.querySelectorAll('.project');
+  
+    projectDivs.forEach((projectDiv) => {
+      const header = projectDiv.querySelector('.project-header');
+      const details = projectDiv.querySelector('.project-details');
+      const media = projectDiv.querySelector('.project-media');
+      const collapseButton = projectDiv.querySelector('.collapse-button');
+  
+      // Add the collapsed class to the elements by default
+      projectDiv.classList.add('collapsed');
+      details.classList.add('collapsed');
+      collapseButton.classList.add('collapsed');
+  
+      collapseButton.addEventListener('click', () => {
+        projectDiv.classList.toggle('collapsed');
+        details.classList.toggle('collapsed');
+        collapseButton.classList.toggle('collapsed');
+      });
+      
+      const mediaItems = projectDiv.querySelectorAll('.project-media img, .project-media video');
+
+        mediaItems.forEach((mediaItem) => {
+        mediaItem.addEventListener('click', () => {
+        mediaItem.classList.toggle('maximized');
+        projectDiv.classList.toggle('media-maximized');
+        document.body.classList.toggle('no-scroll');
+      });
+    });
+      
+    });
+
+    
+    
+  });
+  
