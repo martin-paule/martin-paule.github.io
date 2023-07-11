7b01168c5d36ae3768db1c9c17c7bfdce93347db
Martin Paule <martinpaule17@gmail.com>
2023-07-11 16:20:54 +0100

    bounce anim on media

--- events.js
+++ events.js
@@ -1,19 +1,32 @@
-const bindEvents = ()  => {
-    /* add event listeners*/
+const bindEvents = () => {
   const projectDivs = document.querySelectorAll('.project');
   projectDivs.forEach((projectDiv) => {
+
     const collapseButton = projectDiv.querySelector('.collapse-button');
-    const details = projectDiv.querySelector('.project-details');
 
     collapseButton.addEventListener('click', () => {
+      const details = projectDiv.querySelector('.project-details');
+      const projectMedia = projectDiv.querySelectorAll('.project-media img, .project-media video');
 
+      
+
+      
       projectDiv.classList.toggle('collapsed');
       details.classList.toggle('collapsed');
       collapseButton.classList.toggle('collapsed');
+      
+
+
+
+      projectMedia.forEach((item, index) => {
+        item.style.setProperty('--delay-index', (index));
+      });
+
+      projectDiv.style.maxHeight = projectDiv.scrollHeight + 'px'; // Set max-height to the content's height
+      
 
     });
-
-    
   });
+};
 
-}
+  
